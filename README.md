beb569a23a2910468424ca17882b879666f57a18
fengyun2 <2816708306@qq.com>
2016-10-26 00:47:01 +0800

    webpack-svg-sprites

---
+++ README.md
@@ -0,0 +1,16 @@
+# webpack-svg-sprites
+
+> webpack-svg-sprites
+
+## Install
+
+```bash
+npm install
+webpack
+```
+
+## Run
+
+```bash
+./dist/index.html
+```
---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import indexStyle from '../css/index.css'
+import analytics from '../images/analytics.svg'
+import archives from '../images/archives.svg'
+import businessman from '../images/businessman.svg'
+import businessmen from '../images/businessmen.svg'
+import certificate from '../images/certificate.svg'
+import chat from '../images/chat.svg'
+import contract from '../images/contract.svg'
+
+console.log('import complete')
